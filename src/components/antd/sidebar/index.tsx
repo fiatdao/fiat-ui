819e4b95d0df945d59d4633f53abc5b61fca9f38
fernandomg <fernando.greco@gmail.com>
2021-12-15 18:51:43 -0300

    wip: sidebar footer and refactor

--- src/components/antd/sidebar/index.tsx
+++ src/components/antd/sidebar/index.tsx
@@ -1,36 +1,12 @@
 import s from './s.module.scss'
 import cn from 'classnames'
 import { SiderProps } from 'antd/lib/layout'
-import React, { useState } from 'react'
-import { Button, Layout } from 'antd'
-import Chevron from '@/src/components/assets/svg/chevron.svg'
-import FiatDaoLogo from '@/src/components/assets/svg/fiat-dao-logo.svg'
+import React from 'react'
+import { Layout } from 'antd'
 
 const Sidebar: React.FC<SiderProps> = ({ children, className, ...props }) => {
-  const [collapsed, setCollapsed] = useState(false)
-
   return (
-    <Layout.Sider
-      {...props}
-      breakpoint="lg"
-      className={cn(s.sidebar, className)}
-      collapsed={collapsed}
-      onCollapse={setCollapsed}
-      width={256}
-    >
-      <div className="logo-wrapper">
-        <div className="logo">
-          <FiatDaoLogo />
-          {!collapsed && <p className="app-name">App</p>}
-        </div>
-        <div>
-          <Button
-            icon={<Chevron className={cn('chevron', { collapsed })} />}
-            onClick={() => setCollapsed((prev) => !prev)}
-            type="primary"
-          />
-        </div>
-      </div>
+    <Layout.Sider {...props} breakpoint="lg" className={cn(s.sidebar, className)} width={256}>
       {children}
     </Layout.Sider>
   )
