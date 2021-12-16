ba86f013078577c7a479bd357560201704b94060
Fernando Greco <fernando.greco@gmail.com>
2021-12-16 10:21:58 -0300

    Side Menu styles (#25)

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,6 +1,14 @@
 import { NavLink } from './NavLink'
-import { ReactNode, useState } from 'react'
-import { Menu } from 'antd'
+import cn from 'classnames'
+import { useRouter } from 'next/router'
+import React, { ReactNode, useEffect, useState } from 'react'
+import { Button, Divider, Menu } from 'antd'
+import { ExternalLink as ExternalLinkIcon } from '@/src/components/pureStyledComponents/text/Link'
+import { ExternalLink } from '@/src/components/custom'
+import FiatDaoLogo from '@/src/components/assets/svg/fiat-dao-logo.svg'
+import FiatDaoIcon from '@/src/components/assets/svg/fiat-dao-icon.svg'
+import FiatIcon from '@/src/components/assets/svg/fiat-icon.svg'
+import Chevron from '@/src/components/assets/svg/chevron.svg'
 import DashboardInactive from '@/src/components/assets/svg/dashboard-inactive.svg'
 import DashboardActive from '@/src/components/assets/svg/dashboard-active.svg'
 import OpenPositionInactive from '@/src/components/assets/svg/open-position-inactive.svg'
@@ -19,59 +27,113 @@ type MenuItem = {
   key: string
 }
 
-export const Sidebar = () => {
-  const items: MenuItem[] = [
-    {
-      to: '/dashboard',
-      icon: <DashboardInactive />,
-      iconSelected: <DashboardActive />,
-      title: 'Dashboard',
-      key: 'dashboard',
-    },
-    {
-      to: '/open-position',
-      icon: <OpenPositionInactive />,
-      iconSelected: <OpenPositionActive />,
-      title: 'Open position',
-      key: 'open-position',
-    },
-    {
-      to: '/your-positions',
-      icon: <YourPositionsInactive />,
-      iconSelected: <YourPositionsActive />,
-      title: 'Your Positions',
-      key: 'your-positions',
-    },
-    {
-      to: '/liquidations',
-      icon: <LiquidationsInactive />,
-      iconSelected: <LiquidationsActive />,
-      title: 'Liquidations',
-      key: 'liquidations',
-    },
-  ]
+const items: MenuItem[] = [
+  {
+    to: '/dashboard',
+    icon: <DashboardInactive />,
+    iconSelected: <DashboardActive />,
+    title: 'Dashboard',
+    key: 'dashboard',
+  },
+  {
+    to: '/open-position',
+    icon: <OpenPositionInactive />,
+    iconSelected: <OpenPositionActive />,
+    title: 'Open position',
+    key: 'open-position',
+  },
+  {
+    to: '/your-positions',
+    icon: <YourPositionsInactive />,
+    iconSelected: <YourPositionsActive />,
+    title: 'Your Positions',
+    key: 'your-positions',
+  },
+  {
+    to: '/liquidations',
+    icon: <LiquidationsInactive />,
+    iconSelected: <LiquidationsActive />,
+    title: 'Liquidations',
+    key: 'liquidations',
+  },
+]
 
-  const [selectedItem, setSelectedItem] = useState(items[0].key)
+export const Sidebar = () => {
+  const { pathname } = useRouter()
+  const [selectedItem, setSelectedItem] = useState<MenuItem>()
+  const [collapsed, setCollapsed] = useState(false)
+
+  useEffect(() => {
+    const currentItem = items.find(({ to }) => to === pathname)
+    setSelectedItem(currentItem ?? undefined)
+  }, [pathname])
+
+  const handleAddProjectToken = () => {
+    // TODO: add project token
+  }
 
   return (
-    <Sider>
-      <Menu
-        defaultSelectedKeys={['1']}
-        mode="inline"
-        onSelect={(options) => {
-          console.log({ options })
-          setSelectedItem(options.key)
-        }}
-      >
+    <Sider collapsed={collapsed} onCollapse={setCollapsed}>
+      <div className="logo-wrapper">
+        <div className="logo">
+          <FiatDaoLogo />
+          {!collapsed && <p className="app-name">App</p>}
+        </div>
+        <div>
+          <Button
+            icon={<Chevron className={cn('chevron', { collapsed })} />}
+            onClick={() => setCollapsed((prev) => !prev)}
+            type="primary"
+          />
+        </div>
+      </div>
+      <Menu mode="inline" selectedKeys={[selectedItem?.key ?? '']}>
         {items.map((item) => (
           <Menu.Item
-            icon={selectedItem === item.key ? item.iconSelected : item.icon}
+            icon={selectedItem?.key === item.key ? item.iconSelected : item.icon}
             key={item.key}
           >
             <NavLink href={item.to}>{item.title}</NavLink>
           </Menu.Item>
         ))}
       </Menu>
+      {!collapsed && (
+        <div className="side-menu-footer">
+          <div className="links">
+            <p>
+              <ExternalLink href="https://google.com" title="Buy and sell on Matcha">
+                Buy and sell on Matcha <ExternalLinkIcon />
+              </ExternalLink>
+            </p>
+            <p>
+              <ExternalLink href="https://google.com" title="Borrow and lend on Rari Fuse">
+                Borrow and lend on Rari Fuse <ExternalLinkIcon />
+              </ExternalLink>
+            </p>
+            <p>
+              <ExternalLink href="https://google.com" title="FIAT's Dune Dashboard">
+                FIAT's Dune Dashboard <ExternalLinkIcon />
+              </ExternalLink>
+            </p>
+          </div>
+          <Divider />
+          <div className="add-to-wallet">
+            <p>ADD TO WALLET</p>
+            <div className="buttons-container">
+              <div>
+                <Button onClick={handleAddProjectToken} type="primary">
+                  <FiatIcon /> FIAT
+                </Button>
+              </div>
+              <div>
+                <Button onClick={handleAddProjectToken} type="primary">
+                  <FiatDaoIcon /> FDT
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </Sider>
   )
 }
