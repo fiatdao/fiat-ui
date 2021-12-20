426556c0420b573b33c85085dff5104ee6ef03dd
Gabito Esmiapodo <gabriel@bootnode.dev>
2021-12-20 15:16:12 -0300

    (clean) header

--- src/components/custom/header/index.tsx
+++ src/components/custom/header/index.tsx
@@ -1,23 +1,22 @@
+import s from './s.module.scss'
 import { ConnectButton } from '../connect-button'
 import { Layout } from 'antd'
 import { useRouter } from 'next/router'
+import cn from 'classnames'
 import ConnectedWallet from '@/src/components/custom/connected-wallet'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
-import { Text } from '@/src/components/custom/typography'
 import { routesConfig } from '@/src/constants/navigation'
 
-export const Header: React.FC = () => {
+export const Header: React.FC = ({ ...restProps }) => {
   const { address, isWalletConnected } = useWeb3Connection()
   const router = useRouter()
   const title: string = routesConfig[router.route]?.title || '-'
 
   return (
-    <Layout.Header style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-      <Text color="white" style={{ flex: '1' }} type="h2">
-        {title}
-      </Text>
+    <Layout.Header className={cn(s.component)} {...restProps}>
+      <h1 className={cn(s.title)}>{title}</h1>
       <div>
-        <button>
+        {/* <button>
           <svg height="29" width="36" xmlns="http://www.w3.org/2000/svg">
             <g fill="none" stroke="#fff" strokeWidth="3">
               <path d="M0 1.5h36" />
@@ -25,7 +24,7 @@ export const Header: React.FC = () => {
               <path d="M0 27.5h36" />
             </g>
           </svg>
-        </button>
+        </button> */}
         {isWalletConnected && address ? <ConnectedWallet /> : <ConnectButton />}
       </div>
     </Layout.Header>
