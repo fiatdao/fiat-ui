761e908defe2ba2256e908f3aac9b0c65d970048
Gabito Esmiapodo <gabriel@bootnode.dev>
2021-12-20 16:33:30 -0300

    (polish) header dropdown

--- src/components/custom/connected-wallet/index.tsx
+++ src/components/custom/connected-wallet/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import s from './s.module.scss'
-import { ControlOutlined, DownOutlined, SafetyOutlined, WalletOutlined } from '@ant-design/icons'
 import cn from 'classnames'
 import React from 'react'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
@@ -10,82 +9,65 @@ import Popover from '@/src/components/antd/popover'
 import ExternalLink from '@/src/components/custom/externalLink'
 import Grid from '@/src/components/custom/grid'
 import Identicon from '@/src/components/custom/identicon'
-import { Text } from '@/src/components/custom/typography'
+import AccountImage from '@/src/resources/svg/account_img.svg'
+import ChevronDown from '@/src/resources/svg/chevron-down.svg'
+import Status from '@/src/resources/svg/node-status.svg'
+import Wallet from '@/src/resources/svg/wallet.svg'
+import Network from '@/src/resources/svg/network.svg'
 
 const ConnectedWallet: React.FC = () => {
   const { address, appChainId, disconnectWallet, wallet } = useWeb3Connection()
-
-  const AccountSection = (
-    <Popover
-      className={s.popover}
-      content={
-        <div className="card">
-          <Grid align="center" className="card-header" flow="col" gap={16} justify="start">
-            <Identicon address={address!} height={40} width={40} />
-            <ExternalLink href={getEtherscanAddressUrl(address!)}>
-              <Text className={s.addressStr} tag="p" type="p1" weight="semibold">
-                {shortenAddr(address!, 8, 8)}
-              </Text>
-            </ExternalLink>
-          </Grid>
-          <Grid flow="row" gap={32} padding={[32, 24]}>
-            <Grid colsTemplate="24px 1fr auto" flow="col" gap={16}>
-              {/* <Icon name="node-status" /> */}
-              <SafetyOutlined className={s.icon} size={48} />
-              <Text color="secondary" type="p1">
-                Status
-              </Text>
-              <Text className={s.statusTag} tag="p" type="lb2" weight="semibold">
-                Connected
-              </Text>
-            </Grid>
-            <Grid colsTemplate="24px 1fr auto" flow="col" gap={16}>
-              <WalletOutlined className={s.icon} size={48} />
-              <Text color="secondary" type="p1">
-                Wallet
-              </Text>
-              <Text color="primary" type="p1" weight="semibold">
-                {wallet?.name}
-              </Text>
-            </Grid>
-            <Grid colsTemplate="24px 1fr auto" flow="col" gap={16}>
-              {/* <Icon name="network" /> */}
-              <ControlOutlined className={s.icon} size={48} />
-              <Text color="secondary" type="p1">
-                Network
-              </Text>
-              <Text color="primary" type="p1" weight="semibold">
-                {appChainId}
-              </Text>
-            </Grid>
-          </Grid>
-          <Divider />
-          <Grid padding={24}>
-            <button className="button button-ghost" onClick={disconnectWallet} type="button">
-              <span>Disconnect</span>
-            </button>
-          </Grid>
-        </div>
-      }
-      noPadding
-      placement="bottomRight"
-      trigger="click"
-    >
-      <a className={s.accountLink}>
-        <Grid align="center" flow="col">
-          <Identicon address={address!} className="mr-8" height={24} width={24} />
-          <Text className={cn(s.walletAddress, 'mr-4')} color="primary" type="p1">
-            {shortenAddr(address!, 4, 3)}
-          </Text>
-          <DownOutlined />
-        </Grid>
-      </a>
-    </Popover>
-  )
+  const colstTemplate = '24px 1fr auto'
 
   return (
     <Grid align="center" flow="col" gap={20} justify="center">
-      {AccountSection}
+      <Popover
+        className={s.popover}
+        content={
+          <div className="card">
+            <Grid align="center" className="card-header" flow="col" gap={16} justify="start">
+              <Identicon address={address!} height={40} width={40} />
+              <ExternalLink className={s.addressStr} href={getEtherscanAddressUrl(address!)}>
+                {shortenAddr(address!, 8, 8)}
+              </ExternalLink>
+            </Grid>
+            <Grid flow="row" gap={32} padding={[32, 24]}>
+              <Grid colsTemplate={colstTemplate} flow="col" gap={16}>
+                <Status className={cn(s.itemIcon)} />
+                <span className={cn(s.itemText)}>Status</span>
+                <span className={s.itemStatusBadge}>Connected</span>
+              </Grid>
+              <Grid colsTemplate={colstTemplate} flow="col" gap={16}>
+                <Wallet className={cn(s.itemIcon)} />
+                <span className={cn(s.itemText)}>Wallet</span>
+                <span className={cn(s.itemStatus)}>{wallet?.name}</span>
+              </Grid>
+              <Grid colsTemplate={colstTemplate} flow="col" gap={16}>
+                <Network className={cn(s.itemIcon)} />
+                <span className={cn(s.itemText)}>Network</span>
+                <span className={cn(s.itemStatus)}>{appChainId}</span>
+              </Grid>
+            </Grid>
+            <Divider />
+            <Grid padding={24}>
+              <button className="button button-ghost" onClick={disconnectWallet} type="button">
+                <span>Disconnect</span>
+              </button>
+            </Grid>
+          </div>
+        }
+        noPadding
+        placement="bottomRight"
+        trigger="click"
+      >
+        <button className={cn('button-ghost', s.dropdownButton)}>
+          <span className={cn(s.dropdownButtonContents)}>
+            <AccountImage />
+            <span className={cn(s.walletAddress)}>{shortenAddr(address!, 4, 3)}</span>
+            <ChevronDown className={cn(s.dropdownArrow)} />
+          </span>
+        </button>
+      </Popover>
     </Grid>
   )
 }
