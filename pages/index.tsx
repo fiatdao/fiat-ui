78cb059bf36c421bc88305224a71ba7eb8ce9b8e
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-01-04 17:24:13 -0300

    (fix) dashboard routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,27 @@
-import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import genericSuspense from '@/src/utils/genericSuspense'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 import WalletButton from '@/src/components/custom/connect-button'
-import Spin from '@/src/components/antd/spin'
 
 function Connect({ ...restProps }) {
-  const { isAppConnected } = useWeb3Connection()
-  const [loading, setLoading] = useState(true)
+  const { isWalletConnected } = useWeb3Connection()
 
-  // placeholder to prevent flicker until useSignIn is ready
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false)
-    }, 500)
-
-    return () => {
-      clearTimeout(timeout)
-    }
-  }, [])
-
-  return loading ? (
-    <Spin />
-  ) : (
+  return (
     <div {...restProps}>
-      {!isAppConnected ? (
+      {!isWalletConnected ? (
         <>
           <Head>
             <title>Connect Your Wallet - FIAT</title>
           </Head>
-          <h1>Connect Your Wallet</h1>
-          <p>
-            First we need you to connect your crypto wallet and sign the connection so we can trust
-            it's you and only you that's connected to the app.
-          </p>
+          <h1>Please Connect Your Wallet</h1>
           <WalletButton />
         </>
       ) : (
         <>
           <Head>
-            <title>Sign Connection - FIAT</title>
+            <title>Dashboard - FIAT</title>
           </Head>
-          <h1>Wallet connected, please sign the connection to prove it's you.</h1>
+          <h1>This is the dashboard.</h1>
         </>
       )}
     </div>
