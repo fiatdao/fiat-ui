5afbc71fa83f443ca487cf4936ee06cca98433e1
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-01-07 14:06:38 -0300

    (fix) select chevron

--- src/components/antd/select/index.tsx
+++ src/components/antd/select/index.tsx
@@ -28,16 +28,33 @@ const Select: React.FC<SelectProps<AntdSelectValue>> = <T extends AntdSelectValu
     <AntdSelect<T>
       {...selectProps}
       className={cn(s.button, className)}
-      dropdownClassName={cn(s.dropdown)}
       getPopupContainer={fixScroll ? (trigger) => trigger.parentNode : undefined}
       onChange={() => console.log('onChange')}
       optionLabelProp="label"
+      suffixIcon={
+        loading ? (
+          <AntdSpin size="small" />
+        ) : (
+          <svg
+            fill="none"
+            height="6"
+            viewBox="0 0 10 6"
+            width="10"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.29279 5.29289L0.707007 1.70711C0.0770425 1.07714 0.523209 0 1.41411 0H8.58569C9.47659 0 9.92276 1.07714 9.29279 1.70711L5.70701 5.29289C5.31648 5.68342 4.68332 5.68342 4.29279 5.29289Z"
+              fill="#505050"
+            />
+          </svg>
+        )
+      }
       virtual={false}
     >
       {options.map((item, index) => (
         <AntdSelect.Option
           {...item}
-          className={cn(s.item)}
+          className={cn(s.option)}
           disabled={false}
           key={index}
           value={item.value}
