98d624210b7e4536087a9e32cbf4321e95ccc1bc
LaimeJesus <jesus.laime@camba.coop>
2022-01-19 11:49:14 -0300

    Adds transactions data from SG (#59)
    Co-authored-by: fernandomg <fernando.greco@gmail.com>

--- src/utils/your-positions-api.ts
+++ src/utils/your-positions-api.ts
@@ -15,69 +15,22 @@ export type Position = {
   name: string
   discount: number
   ltv: number
-  lastLTV: number
   minted: number
   maturity: Date
   healthFactor: number | null
   action: string
 }
 
-export type Transaction = {
+export type PositionTransaction = {
   asset: string
   action: string
   amount: number
-  lastAmount: number
+  deltaAmount: number
   transactionHash: string
   date: Date
 }
 
-const SUBGRAPH_API =
-  process.env.SUBGRAPH_API || 'https://api.thegraph.com/subgraphs/name/laimejesus/fiatlux'
-
-const DELAY_TIME = 1000
-
-const TRANSACTION_HISTORY_DATA: Transaction[] = [
-  {
-    asset: 'ePyvUSDC_12_31_21',
-    action: 'Mint',
-    amount: 179264.73,
-    lastAmount: 179264.85,
-    transactionHash: '0xcc09...2bfc',
-    date: new Date(2021, 11, 22),
-  },
-  {
-    asset: 'ePyvUSDC_12_31_21',
-    action: 'Mint',
-    amount: 179264.95,
-    lastAmount: 179264.85,
-    transactionHash: '0xcc09...2bfc',
-    date: new Date(2021, 11, 22),
-  },
-  {
-    asset: 'ePyvUSDC_12_31_21',
-    action: 'Mint',
-    amount: 179264.95,
-    lastAmount: 179264.85,
-    transactionHash: '0xcc09...2bfc',
-    date: new Date(2021, 11, 22),
-  },
-  {
-    asset: 'ePyvUSDC_12_31_21',
-    action: 'Mint',
-    amount: 179264.73,
-    lastAmount: 179264.85,
-    transactionHash: '0xcc09...2bfc',
-    date: new Date(2021, 11, 22),
-  },
-]
-
-const transactionMockFetch = () => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res(TRANSACTION_HISTORY_DATA)
-    }, DELAY_TIME)
-  })
-}
+const SUBGRAPH_API = process.env.NEXT_PUBLIC_REACT_APP_SUBGRAPH_API || ''
 
 const fetchUserProxy = async (userAddress: string) => {
   const keys = '{id,proxyAddress}'
@@ -91,53 +44,82 @@ const fetchUserProxy = async (userAddress: string) => {
     .then((jsonResponse) => jsonResponse.data.userProxy)
 }
 
-const transformPositions = async (positions: any[], provider: any): Promise<Position[]> => {
-  const newPositions = []
-  for (const position of positions) {
-    const vaultAddress = position.vault.address
-    const tokenId = position.tokenId
-    const fiat = bigNumberToDecimal(position.normalDebt)
-    const maturity = await vaultEPTCall(vaultAddress, provider, 'maturity', [tokenId])
-    const trancheAddress = await vaultEPTCall(vaultAddress, provider, 'getTokenAddress', [tokenId])
-    // const underlierToken = await vaultEPTCall(vaultAddress, provider, "underlierToken", null);
-    const discount = await vaultEPTCall(vaultAddress, provider, 'fairPrice', [tokenId, true, true])
-    const name = await trancheCall(trancheAddress!, provider, 'name', null)
-    const decimals = await trancheCall(trancheAddress!, provider, 'decimals', null)
-    // @TODO
-    const ltv = 82
-    const lastLTV = 90
-    const healthFactor = 1
-    const newMaturity = maturity?.mul(1000).toNumber() || Date.now()
+const transformPosition = async (
+  position: any,
+  provider: any,
+): Promise<[Position, PositionTransaction[]]> => {
+  const vaultAddress = position.vault.address
+  const tokenId = position.tokenId
+  // const name = vault.underlyingAsset;
+  const collateral = bigNumberToDecimal(position.collateral)
+  const fiat = bigNumberToDecimal(position.normalDebt)
+  const maturity = await vaultEPTCall(vaultAddress, provider, 'maturity', [tokenId])
+  const trancheAddress = await vaultEPTCall(vaultAddress, provider, 'getTokenAddress', [tokenId])
+  // const underlierToken = await vaultEPTCall(vaultAddress, provider, "underlierToken", null);
+  // const discount = await vaultEPTCall(vaultAddress, provider, 'fairPrice', [tokenId, true, true]);
+  const name = await trancheCall(trancheAddress!, provider, 'name', null)
+  // const decimals = await trancheCall(trancheAddress!, provider, 'decimals', null);
+  // @TODO
+  const ltv = fiat / collateral
+  const healthFactor = 1
+  const newMaturity = maturity?.mul(1000).toNumber() || Date.now()
 
-    const newPosition: Position = {
-      name: name!,
-      discount: bigNumberToDecimal(discount, decimals!),
-      minted: fiat,
-      maturity: new Date(newMaturity),
-      action: 'manage',
-      ltv: ltv,
-      lastLTV: lastLTV,
-      healthFactor,
-    }
-    newPositions.push(newPosition)
+  const newPosition: Position = {
+    name: name!,
+    discount: collateral,
+    minted: fiat,
+    maturity: new Date(newMaturity),
+    action: 'manage',
+    ltv: ltv,
+    healthFactor,
   }
-  return newPositions
+  const newPositionTransactions = []
+  for (const positionTransaction of position.positionTransactions) {
+    const collateral = bigNumberToDecimal(positionTransaction.collateral)
+    const deltaCollateral = bigNumberToDecimal(positionTransaction.deltaCollateral)
+    const newPositionTransaction: PositionTransaction = {
+      asset: name!,
+      action: positionTransaction.type,
+      amount: collateral,
+      deltaAmount: deltaCollateral,
+      transactionHash: positionTransaction.id,
+      date: new Date(newMaturity),
+    }
+    newPositionTransactions.push(newPositionTransaction)
+  }
+  console.log(newPositionTransactions)
+  return [newPosition, newPositionTransactions]
 }
 
 // @TODO: currently working for element-fi
-const fetchPositions = async (userAddress: string, provider: any): Promise<Position[]> => {
+const fetchPositions = async (
+  userAddress: string,
+  provider: any,
+): Promise<[Position[], PositionTransaction[]]> => {
   const userProxy = await fetchUserProxy(userAddress)
+  if (!userProxy) return [[], []]
+
   const proxyAddress = userProxy.proxyAddress.toLowerCase()
-  const keys = '{id,vault{address},tokenId,user,collateral,normalDebt}}'
-  const query = { query: `{positions(where: { user: "${proxyAddress}" })${keys}` }
-  const positions = await fetch(SUBGRAPH_API, {
+  const vaultKeys = 'vault {id,name,address,underlyingAsset}'
+  const transactionKeys = 'positionTransactions {id,type,collateral,normalDebt}'
+  const keys = `{id,tokenId,user,collateral,normalDebt,${vaultKeys},${transactionKeys}}`
+  const query = { query: `{positions(where: { user: "${proxyAddress}" })${keys}}` }
+  const positionsData = await fetch(SUBGRAPH_API, {
     method: 'POST',
     body: JSON.stringify(query),
     headers: { 'Content-Type': 'application/json' },
   })
     .then((response) => response.json())
     .then((jsonResponse) => jsonResponse.data.positions)
-  return transformPositions(positions, provider)
+
+  const positionTransactions: PositionTransaction[] = []
+  const positions = []
+  for (const position of positionsData) {
+    const [newPosition, newPositionTransactions] = await transformPosition(position, provider)
+    positions.push(newPosition)
+    positionTransactions.push(...newPositionTransactions)
+  }
+  return [positions, positionTransactions]
 }
 
 const fetchInfoPage = (positions: any[]): Promise<YourPositionPageInformation> => {
@@ -159,8 +141,8 @@ const fetchInfoPage = (positions: any[]): Promise<YourPositionPageInformation> =
     totalDebt,
     currentValue,
     lowestHealthFactor,
-    nextMaturity: remainingTime(nextMaturity!),
+    nextMaturity: remainingTime(nextMaturity),
   })
 }
 
-export { transactionMockFetch, fetchPositions, fetchInfoPage }
+export { fetchPositions, fetchInfoPage }
