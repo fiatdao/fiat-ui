ccadfbc54239308eb04a71fbe0554e2191f9b5d7
fernandomg <fernando.greco@gmail.com>
2022-01-24 00:05:55 -0300

    `your-positions-api` refactor

--- src/utils/your-positions-api.ts
+++ src/utils/your-positions-api.ts
@@ -1,38 +1,70 @@
 import trancheCall from './callTranche'
 import vaultEPTCall from './callVaultEPT'
 import { bigNumberToDecimal } from './formats'
-import { remainingTime } from './your-positions-utils'
 import { min } from 'date-fns'
+import { Web3Context } from '@/src/providers/web3ConnectionProvider'
 
 export type YourPositionPageInformation = {
   totalDebt: number
   currentValue: number
-  lowestHealthFactor: number | null
-  nextMaturity: string
+  lowestHealthFactor: number
+  nextMaturity: number | null
+}
+
+export type SubgraphPosition = {
+  id: string
+  vault: {
+    id: string
+    address: string
+    name: string
+    underlyingAsset: string
+    originator: string
+    type: string
+    positions: SubgraphPosition[]
+  }
+  tokenId: string
+  user: string
+  collateral: string
+  maxCollateral: string
+  normalDebt: string
+  maxNormalDebt: string
+  positionTransactions: {
+    id: string
+    type: string
+    collateral: string
+    normalDebt: string
+    position: SubgraphPosition
+  }[]
 }
 
 export type Position = {
+  key: string
   name: string
   discount: number
   ltv: number
   minted: number
   maturity: Date
-  healthFactor: number | null
-  action: string
+  healthFactor: number
+  action: {
+    text: string
+    data: Record<string, unknown>
+  }
 }
 
 export type PositionTransaction = {
   asset: string
   action: string
   amount: number
-  deltaAmount: number
+  deltaAmount: number // does not exist in the scheme
   transactionHash: string
   date: Date
 }
 
 const SUBGRAPH_API = process.env.NEXT_PUBLIC_REACT_APP_SUBGRAPH_API || ''
 
-const fetchUserProxy = async (userAddress: string) => {
+const fetchUserProxy = async (
+  userAddress: string,
+): Promise<{ id?: string; proxyAddress?: string }> => {
   const keys = '{id,proxyAddress}'
   const query = { query: `{userProxy(id: "${userAddress.toLowerCase()}")${keys}}` }
   return fetch(SUBGRAPH_API, {
@@ -42,68 +74,100 @@ const fetchUserProxy = async (userAddress: string) => {
   })
     .then((response) => response.json())
     .then((jsonResponse) => jsonResponse.data.userProxy)
+    .catch(() => ({}))
 }
 
+/**
+ * Returns a collection of Position required by the frontend
+ * @param {SubgraphPosition} position
+ * @param {Web3Context['readOnlyAppProvider']} provider
+ * @returns Promise<[Position, Array<PositionTransaction>]>
+ */
 const transformPosition = async (
-  position: any,
-  provider: any,
+  position: SubgraphPosition,
+  provider: Web3Context['readOnlyAppProvider'],
 ): Promise<[Position, PositionTransaction[]]> => {
+  const positionId = position.id
   const vaultAddress = position.vault.address
   const tokenId = position.tokenId
-  // const name = vault.underlyingAsset;
-  const collateral = bigNumberToDecimal(position.collateral)
   const fiat = bigNumberToDecimal(position.normalDebt)
-  const maturity = await vaultEPTCall(vaultAddress, provider, 'maturity', [tokenId])
-  const trancheAddress = await vaultEPTCall(vaultAddress, provider, 'getTokenAddress', [tokenId])
+  const collateral = bigNumberToDecimal(position.collateral)
+
+  const [maturity, trancheAddress] = await Promise.all([
+    vaultEPTCall(vaultAddress, provider, 'maturity', [tokenId]),
+    vaultEPTCall(vaultAddress, provider, 'getTokenAddress', [tokenId]),
+  ])
+
+  const name = await trancheCall(trancheAddress!, provider, 'name', null)
+
+  // const name = vault.underlyingAsset;
   // const underlierToken = await vaultEPTCall(vaultAddress, provider, "underlierToken", null);
   // const discount = await vaultEPTCall(vaultAddress, provider, 'fairPrice', [tokenId, true, true]);
-  const name = await trancheCall(trancheAddress!, provider, 'name', null)
   // const decimals = await trancheCall(trancheAddress!, provider, 'decimals', null);
-  // @TODO
+
   const ltv = fiat / collateral
-  const healthFactor = 1
   const newMaturity = maturity?.mul(1000).toNumber() || Date.now()
 
+  // @TODO: implement calculation for hardcoded values
+  const healthFactor = 1
+
   const newPosition: Position = {
-    name: name!,
+    key: positionId,
+    name: name ?? 'unknown',
     discount: collateral,
     minted: fiat,
     maturity: new Date(newMaturity),
-    action: 'manage',
-    ltv: ltv,
+    action: { text: 'Manage', data: { positionId } },
+    ltv,
     healthFactor,
   }
+
   const newPositionTransactions = []
+
   for (const positionTransaction of position.positionTransactions) {
     const collateral = bigNumberToDecimal(positionTransaction.collateral)
-    const deltaCollateral = bigNumberToDecimal(positionTransaction.deltaCollateral)
+    // const deltaCollateral = bigNumberToDecimal(positionTransaction.deltaCollateral)
     const newPositionTransaction: PositionTransaction = {
-      asset: name!,
+      asset: name ?? 'unknown',
       action: positionTransaction.type,
       amount: collateral,
-      deltaAmount: deltaCollateral,
+      deltaAmount: 0, // FixMe: it must be `deltaCollateral`
       transactionHash: positionTransaction.id,
       date: new Date(newMaturity),
     }
+
     newPositionTransactions.push(newPositionTransaction)
   }
-  console.log(newPositionTransactions)
+
   return [newPosition, newPositionTransactions]
 }
 
-// @TODO: currently working for element-fi
+/**
+ * Fetches position information from the FIAT subgraph
+ *
+ * @todo: support notional-fi protocol
+ * @todo: support barnBridge protocol
+ *
+ * @param {string} userAddress
+ * @param {Web3Context['readOnlyAppProvider']} provider
+ * @returns {Promise<[Position, Array<PositionTransaction>]>}
+ */
 const fetchPositions = async (
   userAddress: string,
-  provider: any,
+  provider: Web3Context['readOnlyAppProvider'],
 ): Promise<[Position[], PositionTransaction[]]> => {
   const userProxy = await fetchUserProxy(userAddress)
-  if (!userProxy) return [[], []]
+  const proxyAddress = userProxy.proxyAddress?.toLowerCase()
 
-  const proxyAddress = userProxy.proxyAddress.toLowerCase()
+  if (!proxyAddress) {
+    return [[], []]
+  }
+
   const vaultKeys = 'vault {id,name,address,underlyingAsset}'
   const transactionKeys = 'positionTransactions {id,type,collateral,normalDebt}'
   const keys = `{id,tokenId,user,collateral,normalDebt,${vaultKeys},${transactionKeys}}`
   const query = { query: `{positions(where: { user: "${proxyAddress}" })${keys}}` }
+
   const positionsData = await fetch(SUBGRAPH_API, {
     method: 'POST',
     body: JSON.stringify(query),
@@ -113,36 +177,73 @@ const fetchPositions = async (
     .then((jsonResponse) => jsonResponse.data.positions)
 
   const positionTransactions: PositionTransaction[] = []
-  const positions = []
+  const positions: Position[] = []
+
   for (const position of positionsData) {
     const [newPosition, newPositionTransactions] = await transformPosition(position, provider)
     positions.push(newPosition)
     positionTransactions.push(...newPositionTransactions)
   }
+
   return [positions, positionTransactions]
 }
 
-const fetchInfoPage = (positions: any[]): Promise<YourPositionPageInformation> => {
-  let totalDebt = 0
-  let currentValue = 0
-  let lowestHealthFactor: number | null = null
-  let nextMaturity: Date | null = null
-  positions.forEach((position) => {
-    totalDebt += position.minted
-    currentValue += position.discount
-    lowestHealthFactor = !lowestHealthFactor
-      ? position.healthFactor
-      : position.healthFactor < lowestHealthFactor
-      ? position.healthFactor
-      : lowestHealthFactor
-    nextMaturity = !nextMaturity ? position.maturity : min([position.maturity, nextMaturity])
+/**
+ * Fetches Position information by its ID
+ *
+ * @param {string} positionId
+ * @param {Web3Context['readOnlyAppProvider']} provider
+ * @returns {Promise<[Position, Array<PositionTransaction>]>}
+ */
+const fetchPositionById = (
+  positionId: string,
+  provider: Web3Context['readOnlyAppProvider'],
+): Promise<[Position, PositionTransaction[]]> => {
+  const vaultKeys = 'vault {id,name,address,underlyingAsset}'
+  const transactionKeys = 'positionTransactions {id,type,collateral,normalDebt}'
+  const keys = `{id,tokenId,user,collateral,normalDebt,${vaultKeys},${transactionKeys}}`
+  const query = { query: `{position(id: "${positionId}")${keys}}` }
+
+  return fetch(SUBGRAPH_API, {
+    method: 'POST',
+    body: JSON.stringify(query),
+    headers: { 'Content-Type': 'application/json' },
   })
-  return Promise.resolve({
-    totalDebt,
-    currentValue,
-    lowestHealthFactor,
-    nextMaturity: remainingTime(nextMaturity),
-  })
+    .then((response) => response.json())
+    .then((jsonResponse) => jsonResponse.data.position)
+    .then((position) => transformPosition(position, provider))
 }
 
-export { fetchPositions, fetchInfoPage }
+/**
+ * Builds the information object for the Info Panels in Your Positions page
+ * @param {Array<Position>} positions
+ * @returns YourPositionPageInformation
+ */
+const fetchInfoPage = (positions: Position[]): YourPositionPageInformation => {
+  const initialPositionInformation: YourPositionPageInformation = {
+    totalDebt: 0,
+    currentValue: 0,
+    lowestHealthFactor: 0,
+    nextMaturity: null,
+  }
+
+  return positions.reduce((acc, { discount, healthFactor, maturity, minted }) => {
+    // totalDebt
+    const totalDebt = acc.totalDebt + minted
+    // currentValue
+    const currentValue = acc.currentValue + discount
+
+    // lowestHealthFactor
+    const isNewLowest = acc.lowestHealthFactor === null || healthFactor < acc.lowestHealthFactor
+    const lowestHealthFactor = isNewLowest ? healthFactor : acc.lowestHealthFactor
+
+    // nextMaturity
+    const maturityInMS = maturity.getTime()
+    const nextMaturity =
+      acc.nextMaturity === null ? maturityInMS : min([maturityInMS, acc.nextMaturity]).getTime()
+
+    return { totalDebt, currentValue, lowestHealthFactor, nextMaturity }
+  }, initialPositionInformation)
+}
+
+export { fetchPositionById, fetchPositions, fetchInfoPage }
