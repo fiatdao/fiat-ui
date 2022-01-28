1907c194f570ce5626f6c4baa4f87dfd5b624e74
fernandomg <fernando.greco@gmail.com>
2022-01-28 20:26:15 -0300

    Manage - Collateral Withdraw

--- pages/your-positions/[positionId]/manage/index.tsx
+++ pages/your-positions/[positionId]/manage/index.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { ReactNode, useEffect, useState } from 'react'
 import BigNumber from 'bignumber.js'
-import useSWR from 'swr'
+import useSWR, { KeyedMutator } from 'swr'
 import { getHumanValue, getNonHumanValue } from '@/src/web3/utils'
-import { TestERC20 } from '@/types/typechain'
+import { TestERC20, VaultEPT } from '@/types/typechain'
 import useContractCall from '@/src/hooks/contracts/useContractCall'
 import { contracts } from '@/src/constants/contracts'
 import useUserProxy from '@/src/hooks/useUserProxy'
@@ -16,7 +16,7 @@ import ElementIcon from '@/src/resources/svg/element.svg'
 import FiatIcon from '@/src/resources/svg/fiat-icon.svg'
 import { Form } from '@/src/components/antd'
 import { Tab, Tabs, TokenAmount } from '@/src/components/custom'
-import { fetchPositionById } from '@/src/utils/your-positions-api'
+import { Position, fetchPositionById } from '@/src/utils/your-positions-api'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 import genericSuspense from '@/src/utils/genericSuspense'
 
@@ -34,9 +34,9 @@ const isValidPositionId = (positionId: string | string[] | undefined): boolean =
 }
 
 const extractPositionIdData = (
-  positionId: string,
+  positionId?: string,
 ): { vaultAddress: string; tokenId: string; proxyAddress: string } => {
-  if (!isValidPositionId(positionId)) {
+  if (!isValidPositionIdType(positionId) || !isValidPositionId(positionId)) {
     const error = new Error('Invalid position Id')
     error.name = 'INVALID_POSITION_ID'
 
@@ -62,23 +62,64 @@ const iconByAddress = new Proxy<Record<string, ReactNode>>(
   },
 )
 
-const DepositInput = ({
+const useManagePositionInfo = () => {
+  const {
+    query: { positionId },
+  } = useRouter()
+  const { isWalletConnected, readOnlyAppProvider: provider } = useWeb3Connection()
+
+  const { data, error, mutate } = useSWR([positionId], () => {
+    if (
+      isWalletConnected &&
+      provider &&
+      isValidPositionIdType(positionId) &&
+      isValidPositionId(positionId)
+    ) {
+      return fetchPositionById(positionId, provider).then(([position]) => position)
+    }
+  })
+
+  if (error) {
+    console.error('Failed to retrieve PositionId information', error)
+  }
+
+  return { data, mutate }
+}
+
+const DepositForm = ({
   tokenAddress,
-  tokenDecimals,
-  tokenMaxValue,
   vaultAddress,
 }: {
   tokenAddress: string
-  tokenDecimals: number
-  tokenMaxValue?: BigNumber
   vaultAddress: string
 }) => {
+  const { address: currentUserAddress, readOnlyAppProvider: provider } = useWeb3Connection()
   const { userProxy } = useUserProxy()
   const userActions = useUserActions()
   const [form] = AntdForm.useForm()
 
+  const [tokenInfo, setTokenInfo] = useState<{
+    decimals: number
+    humanValue?: BigNumber
+  }>({ decimals: 0 })
+
+  useEffect(() => {
+    if (currentUserAddress) {
+      const collateral = new Contract(tokenAddress, contracts.TEST_ERC20.abi, provider) as TestERC20
+
+      Promise.all([collateral.decimals(), collateral.balanceOf(currentUserAddress)]).then(
+        ([decimals, balance]) => {
+          setTokenInfo({
+            decimals,
+            humanValue: getHumanValue(BigNumber.from(balance.toString()), decimals),
+          })
+        },
+      )
+    }
+  }, [tokenAddress, currentUserAddress, provider])
+
   const handleDeposit = async ({ deposit }: { deposit: BigNumber }) => {
-    const toDeposit = getNonHumanValue(deposit, tokenDecimals)
+    const toDeposit = getNonHumanValue(deposit, tokenInfo.decimals)
     const addCollateralEncoded = userActions.interface.encodeFunctionData('addCollateral', [
       vaultAddress,
       tokenAddress,
@@ -86,7 +127,7 @@ const DepositInput = ({
     ])
 
     const tx = await userProxy?.execute(userActions.address, addCollateralEncoded, {
-      gasLimit: 10000000,
+      gasLimit: 1_000_000,
     })
     console.log('adding collateral...', tx.hash)
 
@@ -98,9 +139,9 @@ const DepositInput = ({
     <Form form={form} onFinish={handleDeposit}>
       <Form.Item name="deposit" required>
         <TokenAmount
-          displayDecimals={tokenDecimals}
-          max={tokenMaxValue}
-          maximumFractionDigits={tokenDecimals}
+          displayDecimals={tokenInfo.decimals}
+          max={tokenInfo.humanValue}
+          maximumFractionDigits={tokenInfo.decimals}
           slider
           tokenIcon={iconByAddress[tokenAddress]}
         />
@@ -108,6 +149,86 @@ const DepositInput = ({
       <Form.Item>
         <Button htmlType="submit" type="primary">
           Deposit collateral
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+const WithdrawForm = ({
+  refetch,
+  tokenAddress,
+  tokenId,
+  userBalance,
+  vaultAddress,
+}: {
+  refetch: KeyedMutator<Position | undefined>
+  tokenAddress: string
+  tokenId: string
+  userBalance?: number
+  vaultAddress: string
+}) => {
+  const { address: currentUserAddress, readOnlyAppProvider: provider } = useWeb3Connection()
+  const { userProxy } = useUserProxy()
+  const userActions = useUserActions()
+  const [form] = AntdForm.useForm()
+
+  const [wrappedCollateralInfo, setWrappedCollateralInfo] = useState<{
+    decimals: number
+    humanValue?: BigNumber
+  }>()
+
+  useEffect(() => {
+    if (currentUserAddress) {
+      const vaultEPT = new Contract(vaultAddress, contracts.VAULT_EPT.abi, provider) as VaultEPT
+
+      Promise.all([vaultEPT.dec(), vaultEPT.balanceOf(currentUserAddress, tokenId)]).then(
+        ([decimals, balance]) => {
+          setWrappedCollateralInfo({
+            decimals: decimals.toNumber(),
+            humanValue: getHumanValue(BigNumber.from(balance.toString()), decimals.toNumber()),
+          })
+        },
+      )
+    }
+  }, [currentUserAddress, provider, tokenId, vaultAddress])
+
+  const handleWithdraw = async ({ withdraw }: { withdraw: BigNumber }) => {
+    if (wrappedCollateralInfo) {
+      const toWithdraw = getNonHumanValue(withdraw, wrappedCollateralInfo.decimals)
+      const removeCollateralEncoded = userActions.interface.encodeFunctionData('removeCollateral', [
+        vaultAddress,
+        tokenAddress,
+        toWithdraw.toFixed(),
+      ])
+
+      const tx = await userProxy?.execute(userActions.address, removeCollateralEncoded, {
+        gasLimit: 1_000_000,
+      })
+      console.log('withdrawing...', tx.hash)
+
+      const receipt = await tx.wait()
+      console.log('Collateral withdrawn', { receipt })
+
+      refetch()
+    }
+  }
+
+  return (
+    <Form form={form} onFinish={handleWithdraw}>
+      <Form.Item name="withdraw" required>
+        <TokenAmount
+          disabled={false}
+          displayDecimals={wrappedCollateralInfo?.decimals}
+          max={userBalance}
+          maximumFractionDigits={wrappedCollateralInfo?.decimals}
+          slider
+          tokenIcon={iconByAddress[tokenAddress]}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit" type="primary">
+          Withdraw collateral
         </Button>
       </Form.Item>
     </Form>
@@ -136,12 +257,12 @@ const isFiatTab = (key: string): key is ManageFiatProps['activeTabKey'] => {
   return FIAT_KEYS.includes(key as ManageFiatProps['activeTabKey'])
 }
 
-const ManageCollateral = ({ activeTabKey = 'deposit', setActiveTabKey }: ManageCollateralProps) => {
-  const { address: currentUserAddress, readOnlyAppProvider: provider } = useWeb3Connection()
-  const {
-    query: { positionId },
-  } = useRouter()
-  const { tokenId, vaultAddress } = extractPositionIdData(positionId as string)
+const ManageCollateral = ({ activeTabKey, setActiveTabKey }: ManageCollateralProps) => {
+  const { data: position, mutate: refetchPosition } = useManagePositionInfo()
+
+  const { tokenId, vaultAddress } = extractPositionIdData(
+    position?.action?.data?.positionId as string,
+  )
 
   const [collateralAddress] = useContractCall(
     vaultAddress,
@@ -149,37 +270,6 @@ const ManageCollateral = ({ activeTabKey = 'deposit', setActiveTabKey }: ManageC
     'getTokenAddress',
     [tokenId],
   )
-
-  const [collateralInfo, setCollateralInfo] = useState<{
-    decimals: number
-    humanValue?: BigNumber
-  }>({ decimals: 0 })
-
-  useEffect(() => {
-    if (collateralAddress && currentUserAddress) {
-      const collateral = new Contract(
-        collateralAddress,
-        contracts.TEST_ERC20.abi,
-        provider,
-      ) as TestERC20
-
-      Promise.all([collateral.decimals(), collateral.balanceOf(currentUserAddress)]).then(
-        ([decimals, balance]) => {
-          setCollateralInfo({
-            decimals,
-            humanValue: balance
-              ? getHumanValue(BigNumber.from(balance.toString()), decimals)
-              : undefined,
-          })
-        },
-      )
-    }
-  }, [collateralAddress, currentUserAddress, provider])
-
-  const [form2] = AntdForm.useForm()
-  const handleWithdraw = ({ withdraw }: { withdraw: string }) => {
-    console.log(withdraw)
-  }
 
   return (
     <>
@@ -192,72 +282,23 @@ const ManageCollateral = ({ activeTabKey = 'deposit', setActiveTabKey }: ManageC
         </Tab>
       </Tabs>
       {'deposit' === activeTabKey && (
-        <div>
-          <DepositInput
-            tokenAddress={collateralAddress}
-            tokenDecimals={collateralInfo.decimals}
-            tokenMaxValue={collateralInfo.humanValue}
-            vaultAddress={vaultAddress}
-          />
-        </div>
+        <DepositForm tokenAddress={collateralAddress} vaultAddress={vaultAddress} />
       )}
       {'withdraw' === activeTabKey && (
-        <div>
-          <Form
-            form={form2}
-            initialValues={{ tokenAmount: 0, fiatAmount: 0 }}
-            onFinish={handleWithdraw}
-            validateTrigger={['onSubmit']}
-          >
-            <Form.Item name="withdraw" required>
-              <TokenAmount
-                disabled={false}
-                displayDecimals={4}
-                max={5000}
-                maximumFractionDigits={6}
-                slider
-                tokenIcon={<ElementIcon />}
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button htmlType="submit" type="primary">
-                Withdraw collateral
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
+        <WithdrawForm
+          refetch={refetchPosition}
+          tokenAddress={collateralAddress}
+          tokenId={tokenId}
+          userBalance={position?.discount}
+          vaultAddress={vaultAddress}
+        />
       )}
+      <pre>{JSON.stringify(position, null, 2)}</pre>
     </>
   )
 }
 
-const useManagePositionInfo = () => {
-  const {
-    query: { positionId },
-  } = useRouter()
-  const { isWalletConnected, readOnlyAppProvider: provider } = useWeb3Connection()
-
-  const { data, error, mutate } = useSWR([positionId], () => {
-    if (
-      isWalletConnected &&
-      provider &&
-      isValidPositionIdType(positionId) &&
-      isValidPositionId(positionId)
-    ) {
-      return fetchPositionById(positionId, provider)
-    }
-  })
-
-  if (error) {
-    console.error('Failed to retrieve PositionId information', error)
-  }
-
-  return [data, mutate]
-}
-
 const PositionManager = () => {
-  const [positionInfo] = useManagePositionInfo()
-
   const [activeSection, setActiveSection] = useState<'collateral' | 'fiat'>('collateral')
   const [activeTabKey, setActiveTabKey] = useState<
     ManageCollateralProps['activeTabKey'] | ManageFiatProps['activeTabKey']
@@ -360,7 +401,6 @@ const PositionManager = () => {
           )}
         </>
       )}
-      <pre>{JSON.stringify(positionInfo, null, 2)}</pre>
     </>
   )
 }
