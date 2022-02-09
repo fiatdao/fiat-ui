cdf7dceebcdb8378613f5bf4e9864792b5599c92
Gabito Esmiapodo <gabriel@bootnode.dev>
2022-02-09 15:42:24 -0300

    (add) last step stuff

--- pages/open-position/[collateralId]/open/index.tsx
+++ pages/open-position/[collateralId]/open/index.tsx
@@ -20,6 +20,7 @@ import ElementIcon from '@/src/resources/svg/element.svg'
 import FiatIcon from '@/src/resources/svg/fiat-icon.svg'
 import Plus from '@/src/resources/svg/gradient-plus.svg'
 import Less from '@/src/resources/svg/gradient-less.svg'
+import Success from '@/src/resources/svg/success.svg'
 import useUserProxy from '@/src/hooks/useUserProxy'
 import useContractCall from '@/src/hooks/contracts/useContractCall'
 import { ERC20 } from '@/types/typechain'
@@ -46,33 +47,44 @@ const StepperTitle: React.FC<{
   </div>
 )
 
-// const OpenPositionSummary: React.FC<{
-//   currentCollateralValue: number
-//   outstandingFIATDebt: number
-//   newFIATDebt: number
-//   stabilityFee: number
-// }> = ({ currentCollateralValue, newFIATDebt, outstandingFIATDebt, stabilityFee }) => {
-//   return (
-//     <Grid className={s.summary} flow="row" gap={8}>
-//       <Grid colsTemplate="auto auto" flow="col">
-//         <div className={s.summaryTitle}>Current collateral value</div>
-//         <div className={s.summaryValue}>{currentCollateralValue}</div>
-//       </Grid>
-//       <Grid colsTemplate="auto auto" flow="col">
-//         <div className={s.summaryTitle}>Outstanding FIAT debt</div>
-//         <div className={s.summaryValue}>{outstandingFIATDebt}</div>
-//       </Grid>
-//       <Grid colsTemplate="auto auto" flow="col">
-//         <div className={s.summaryTitle}>New FIAT debt</div>
-//         <div className={s.summaryValue}>{newFIATDebt}</div>
-//       </Grid>
-//       <Grid colsTemplate="auto auto" flow="col">
-//         <div className={s.summaryTitle}>Stability fee</div>
-//         <div className={s.summaryValue}>{stabilityFee}</div>
-//       </Grid>
-//     </Grid>
-//   )
-// }
+const Summary: React.FC = () => {
+  const hfState = 'ok'
+
+  return (
+    <div className={s.summary}>
+      <div className={s.summaryRow}>
+        <div className={s.summaryTitle}>In your wallet</div>
+        <div className={s.summaryValue}>5,000 DAI Principal Token</div>
+      </div>
+      <div className={s.summaryRow}>
+        <div className={s.summaryTitle}>Depositing into position </div>
+        <div className={s.summaryValue}>5,000 DAI Principal Token</div>
+      </div>
+      <div className={s.summaryRow}>
+        <div className={s.summaryTitle}>Remaining in wallet</div>
+        <div className={s.summaryValue}>0 DAI Principal Token</div>
+      </div>
+      <div className={s.summaryRow}>
+        <div className={s.summaryTitle}>FIAT to be minted</div>
+        <div className={s.summaryValue}>2,000</div>
+      </div>
+      <div className={s.summaryRow}>
+        <div className={s.summaryTitle}>Updated health factor</div>
+        <div
+          className={cn(
+            s.summaryValue,
+            { [s.ok]: hfState === 'ok' },
+            // TODO: Make these work
+            // { [s.warning]: hfState === 'warning' },
+            // { [s.danger]: hfState === 'danger' }
+          )}
+        >
+          2.3
+        </div>
+      </div>
+    </div>
+  )
+}
 
 type FormProps = { tokenAmount: BigNumber; fiatAmount: BigNumber }
 
@@ -81,7 +93,6 @@ const FormERC20: React.FC<{ tokenSymbol: string; tokenAddress: string }> = ({
   tokenSymbol,
 }) => {
   const [form] = AntdForm.useForm<FormProps>()
-
   const { address: currentUserAddress, isAppConnected, web3Provider } = useWeb3Connection()
   const { isProxyAvailable, loadingProxy, setupProxy, userProxy, userProxyAddress } = useUserProxy()
   const { allowance, approve, hasAllowance, loadingApprove } = useERC20Allowance(
@@ -124,199 +135,174 @@ const FormERC20: React.FC<{ tokenSymbol: string; tokenAddress: string }> = ({
   const [mintFiat, setMintFiat] = useState(false)
 
   const toggleMintFiat = () => setMintFiat(!mintFiat)
-  const hfState = 'ok'
 
   return (
     <div className={cn(s.formWrapper)}>
-      <StepperTitle
-        currentStep={stateMachine.context.currentStepNumber}
-        description={TITLES_BY_STEP[stateMachine.context.currentStepNumber].subtitle}
-        title={TITLES_BY_STEP[stateMachine.context.currentStepNumber].title}
-        totalSteps={stateMachine.context.totalStepNumber}
-      />
-      <div className={cn(s.form)}>
-        {stateMachine.context.currentStepNumber === 1 && (
-          <RadioTabsWrapper className={cn(s.radioTabsWrapper)}>
-            <RadioTab checked={tab === 'bond'} onClick={() => setTab('bond')}>
-              Bond
-            </RadioTab>
-            <RadioTab checked={tab === 'underlying'} onClick={() => setTab('underlying')}>
-              Underlying
-            </RadioTab>
-          </RadioTabsWrapper>
-        )}
-        {[1, 4].includes(stateMachine.context.currentStepNumber) && (
-          <div className={cn(s.balanceWrapper)}>
-            <h3 className={cn(s.balanceLabel)}>Deposit {stateMachine.context.tokenSymbol}</h3>
-            <p className={cn(s.balance)}>Available: {humanReadableValue?.toFixed()}</p>
-          </div>
-        )}
-        <Form form={form} initialValues={{ tokenAmount: 0, fiatAmount: 0 }}>
-          {[1, 4].includes(stateMachine.context.currentStepNumber) && (
-            <Form.Item name="tokenAmount" required>
-              <TokenAmount
-                displayDecimals={4}
-                max={humanReadableValue}
-                maximumFractionDigits={6}
-                onChange={(val) => val && send({ type: 'SET_ERC20_AMOUNT', erc20Amount: val })}
-                slider
-                tokenIcon={<ElementIcon />}
-              />
-            </Form.Item>
-          )}
-          {stateMachine.context.currentStepNumber === 1 && (
-            <div className={s.buttonsWrapper}>
-              {!isProxyAvailable && (
-                <ButtonGradient
-                  disabled={!stateMachine.context.erc20Amount.gt(0)}
-                  height="lg"
-                  onClick={() => send({ type: 'CLICK_SETUP_PROXY' })}
-                >
-                  Setup Proxy
-                </ButtonGradient>
+      {stateMachine.context.currentStepNumber !== 7 ? (
+        <>
+          <StepperTitle
+            currentStep={stateMachine.context.currentStepNumber}
+            description={TITLES_BY_STEP[stateMachine.context.currentStepNumber].subtitle}
+            title={TITLES_BY_STEP[stateMachine.context.currentStepNumber].title}
+            totalSteps={stateMachine.context.totalStepNumber}
+          />
+          <div className={cn(s.form)}>
+            {stateMachine.context.currentStepNumber === 1 && (
+              <RadioTabsWrapper className={cn(s.radioTabsWrapper)}>
+                <RadioTab checked={tab === 'bond'} onClick={() => setTab('bond')}>
+                  Bond
+                </RadioTab>
+                <RadioTab checked={tab === 'underlying'} onClick={() => setTab('underlying')}>
+                  Underlying
+                </RadioTab>
+              </RadioTabsWrapper>
+            )}
+            {[1, 4].includes(stateMachine.context.currentStepNumber) && (
+              <div className={cn(s.balanceWrapper)}>
+                <h3 className={cn(s.balanceLabel)}>Deposit {stateMachine.context.tokenSymbol}</h3>
+                <p className={cn(s.balance)}>Available: {humanReadableValue?.toFixed()}</p>
+              </div>
+            )}
+            <Form form={form} initialValues={{ tokenAmount: 0, fiatAmount: 0 }}>
+              {[1, 4].includes(stateMachine.context.currentStepNumber) && (
+                <Form.Item name="tokenAmount" required>
+                  <TokenAmount
+                    displayDecimals={4}
+                    max={humanReadableValue}
+                    maximumFractionDigits={6}
+                    onChange={(val) => val && send({ type: 'SET_ERC20_AMOUNT', erc20Amount: val })}
+                    slider
+                    tokenIcon={<ElementIcon />}
+                  />
+                </Form.Item>
               )}
-              {!hasAllowance && (
-                <ButtonGradient
-                  disabled={!stateMachine.context.erc20Amount.gt(0) || !isProxyAvailable}
-                  height="lg"
-                  onClick={() => send({ type: 'CLICK_ALLOW' })}
-                >
-                  Set Allowance
-                </ButtonGradient>
+              {stateMachine.context.currentStepNumber === 1 && (
+                <div className={s.buttonsWrapper}>
+                  {!isProxyAvailable && (
+                    <ButtonGradient
+                      disabled={!stateMachine.context.erc20Amount.gt(0)}
+                      height="lg"
+                      onClick={() => send({ type: 'CLICK_SETUP_PROXY' })}
+                    >
+                      Setup Proxy
+                    </ButtonGradient>
+                  )}
+                  {!hasAllowance && (
+                    <ButtonGradient
+                      disabled={!stateMachine.context.erc20Amount.gt(0) || !isProxyAvailable}
+                      height="lg"
+                      onClick={() => send({ type: 'CLICK_ALLOW' })}
+                    >
+                      Set Allowance
+                    </ButtonGradient>
+                  )}
+                </div>
               )}
-            </div>
-          )}
-          {stateMachine.context.currentStepNumber === 2 && (
-            <div className={s.buttonsWrapper}>
-              <ButtonGradient height="lg" loading={loadingProxy} onClick={setupProxy}>
-                Create Proxy
-              </ButtonGradient>
-              <button className={s.backButton} onClick={() => console.log('go back')}>
-                &#8592; Go back
-              </button>
-            </div>
-          )}
-          {stateMachine.context.currentStepNumber === 3 && (
-            <ButtonGradient height="lg" loading={loadingApprove} onClick={approve}>
-              Set Allowance for TOKEN_NAME
-            </ButtonGradient>
-          )}
-          {stateMachine.context.currentStepNumber === 4 && (
-            <>
-              {mintFiat && (
-                <div className={cn(s.fiatWrapper)}>
-                  <button className={cn(s.fiatWrapperTop)} onClick={() => toggleMintFiat()}>
-                    <span className={cn(s.fiatWrapperTopInner)}>
-                      <Less />
-                      <span>Mint FIAT with this transaction</span>
-                    </span>
+              {stateMachine.context.currentStepNumber === 2 && (
+                <div className={s.buttonsWrapper}>
+                  <ButtonGradient height="lg" loading={loadingProxy} onClick={setupProxy}>
+                    Create Proxy
+                  </ButtonGradient>
+                  <button className={s.backButton} onClick={() => console.log('go back')}>
+                    &#8592; Go back
                   </button>
-                  <div className={cn(s.fiatWrapperContents)}>
-                    <div className={cn(s.fiatWrapperContentsInner)}>
-                      <div className={cn(s.balanceWrapper)}>
-                        <h3 className={cn(s.balanceLabel)}>Mint FIAT</h3>
-                        <p className={cn(s.balance)}>Available: 4,800</p>
-                      </div>
-                      <Form.Item name="fiatAmount" required style={{ marginBottom: 0 }}>
-                        <TokenAmount
-                          disabled={false}
-                          displayDecimals={4}
-                          max={stateMachine.context.erc20Amount.toNumber()}
-                          maximumFractionDigits={6}
-                          onChange={(val) =>
-                            val && send({ type: 'SET_FIAT_AMOUNT', fiatAmount: val })
-                          }
-                          slider="healthFactorVariant"
-                          tokenIcon={<FiatIcon />}
-                        />
-                      </Form.Item>
-                    </div>
-                  </div>
                 </div>
               )}
-              <div className={s.buttonsWrapper}>
-                {!mintFiat && (
-                  <ButtonOutlineGradient onClick={() => toggleMintFiat()} textGradient>
-                    <Plus />
-                    Mint FIAT with this transaction
-                  </ButtonOutlineGradient>
-                )}
-                <ButtonGradient height="lg" onClick={() => send({ type: 'CLICK_DEPLOY' })}>
-                  Deposit collateral
+              {stateMachine.context.currentStepNumber === 3 && (
+                <ButtonGradient height="lg" loading={loadingApprove} onClick={approve}>
+                  Set Allowance for TOKEN_NAME
                 </ButtonGradient>
-              </div>
-            </>
-          )}
-          {/* Not sure if this is included anymore... */}
-          {/* {[1, 4, 5].includes(stateMachine.context.currentStepNumber) && (
-            <OpenPositionSummary
-              currentCollateralValue={stateMachine.context.erc20Amount.toNumber()}
-              newFIATDebt={0}
-              outstandingFIATDebt={0}
-              stabilityFee={0}
-            />
-          )} */}
-          {stateMachine.context.currentStepNumber === 5 && (
-            <>
-              <div className={s.summary}>
-                <div className={s.summaryRow}>
-                  <div className={s.summaryRowTitle}>In your wallet</div>
-                  <div className={s.summaryRowValue}>5,000 DAI Principal Token</div>
-                </div>
-                <div className={s.summaryRow}>
-                  <div className={s.summaryRowTitle}>Depositing into position </div>
-                  <div className={s.summaryRowValue}>5,000 DAI Principal Token</div>
-                </div>
-                <div className={s.summaryRow}>
-                  <div className={s.summaryRowTitle}>Remaining in wallet</div>
-                  <div className={s.summaryRowValue}>0 DAI Principal Token</div>
-                </div>
-                <div className={s.summaryRow}>
-                  <div className={s.summaryRowTitle}>FIAT to be minted</div>
-                  <div className={s.summaryRowValue}>2,000</div>
-                </div>
-                <div className={s.summaryRow}>
-                  <div className={s.summaryRowTitle}>Updated health factor</div>
-                  <div
-                    className={cn(
-                      s.summaryRowValue,
-                      { [s.ok]: hfState === 'ok' },
-                      // { [s.warning]: hfState === 'warning' },
-                      // { [s.danger]: hfState === 'danger' }
+              )}
+              {stateMachine.context.currentStepNumber === 4 && (
+                <>
+                  {mintFiat && (
+                    <div className={cn(s.fiatWrapper)}>
+                      <button className={cn(s.fiatWrapperTop)} onClick={() => toggleMintFiat()}>
+                        <span className={cn(s.fiatWrapperTopInner)}>
+                          <Less />
+                          <span>Mint FIAT with this transaction</span>
+                        </span>
+                      </button>
+                      <div className={cn(s.fiatWrapperContents)}>
+                        <div className={cn(s.fiatWrapperContentsInner)}>
+                          <div className={cn(s.balanceWrapper)}>
+                            <h3 className={cn(s.balanceLabel)}>Mint FIAT</h3>
+                            <p className={cn(s.balance)}>Available: 4,800</p>
+                          </div>
+                          <Form.Item name="fiatAmount" required style={{ marginBottom: 0 }}>
+                            <TokenAmount
+                              disabled={false}
+                              displayDecimals={4}
+                              max={stateMachine.context.erc20Amount.toNumber()}
+                              maximumFractionDigits={6}
+                              onChange={(val) =>
+                                val && send({ type: 'SET_FIAT_AMOUNT', fiatAmount: val })
+                              }
+                              slider="healthFactorVariant"
+                              tokenIcon={<FiatIcon />}
+                            />
+                          </Form.Item>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  <div className={s.buttonsWrapper}>
+                    {!mintFiat && (
+                      <ButtonOutlineGradient onClick={() => toggleMintFiat()} textGradient>
+                        <Plus />
+                        Mint FIAT with this transaction
+                      </ButtonOutlineGradient>
                     )}
-                  >
-                    2.3
+                    <ButtonGradient height="lg" onClick={() => send({ type: 'CLICK_DEPLOY' })}>
+                      Deposit collateral
+                    </ButtonGradient>
                   </div>
-                </div>
-              </div>
-              <div className={s.buttonsWrapper}>
-                <ButtonGradient
-                  disabled={!hasAllowance || !isProxyAvailable}
-                  height="lg"
-                  onClick={() =>
-                    send({
-                      type: 'CONFIRM',
-                      // @ts-ignore TODO types
-                      currentUserAddress,
-                      isAppConnected,
-                      web3Provider,
-                      userActions,
-                      userProxy,
-                      refetchErc20Balance,
-                      allowance,
-                    })
-                  }
-                >
-                  Confirm
-                </ButtonGradient>
-                <button className={s.backButton} onClick={() => console.log('go back')}>
-                  &#8592; Go back
-                </button>
-              </div>
-            </>
-          )}
-        </Form>
-      </div>
+                </>
+              )}
+              {stateMachine.context.currentStepNumber === 5 && (
+                <>
+                  <Summary />
+                  <div className={s.buttonsWrapper}>
+                    <ButtonGradient
+                      disabled={!hasAllowance || !isProxyAvailable}
+                      height="lg"
+                      onClick={() =>
+                        send({
+                          type: 'CONFIRM',
+                          // @ts-ignore TODO types
+                          currentUserAddress,
+                          isAppConnected,
+                          web3Provider,
+                          userActions,
+                          userProxy,
+                          refetchErc20Balance,
+                          allowance,
+                        })
+                      }
+                    >
+                      Confirm
+                    </ButtonGradient>
+                    <button className={s.backButton} onClick={() => console.log('go back')}>
+                      &#8592; Go back
+                    </button>
+                  </div>
+                </>
+              )}
+            </Form>
+          </div>
+        </>
+      ) : (
+        <div className={cn(s.form)}>
+          {/* Note: this will be replaced with the actual animation */}
+          <div className={cn(s.lastStepAnimation)}>
+            <Success />
+          </div>
+          <h1 className={cn(s.lastStepTitle)}>Congrats!</h1>
+          <p className={cn(s.lastStepText)}>Your position has been successfully created.</p>
+          <Summary />
+          <ButtonGradient height="lg">Go to bb_sBond_cDAI position</ButtonGradient>
+        </div>
+      )}
     </div>
   )
 }
