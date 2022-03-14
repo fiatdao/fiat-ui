4eccf6e8611140dd03ad68da731c3cb02f83ca1c
LaimeJesus <jesus.laime@camba.coop>
2022-03-14 13:46:20 -0300

    fixes: position creation it was using and incorrect user action (#193)
    removes unused abis

--- src/hooks/useUserActions.ts
+++ src/hooks/useUserActions.ts
@@ -6,7 +6,7 @@ import BigNumber from 'bignumber.js'
 import { contracts } from '@/src/constants/contracts'
 import { Chains } from '@/src/constants/chains'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
-import { Vault20Actions } from '@/types/typechain'
+import { VaultEPTActions } from '@/types/typechain'
 
 type ModifyCollateralAndDebt = {
   vault: string
@@ -41,7 +41,6 @@ export type BurnFIAT = BaseModify & {
 }
 
 type UseUserActions = {
-  userAction: Vault20Actions
   approveFIAT: (to: string) => Promise<void>
   depositCollateral: (arg0: DepositCollateral) => Promise<void>
   withdrawCollateral: (arg0: WithdrawCollateral) => Promise<void>
@@ -53,31 +52,41 @@ export const useUserActions = (): UseUserActions => {
   const { address, web3Provider } = useWeb3Connection()
   const { userProxy, userProxyAddress } = useUserProxy()
 
-  const userAction = useMemo(() => {
+  // Element User Action: ERC20
+  const userActionEPT = useMemo(() => {
     return new Contract(
-      // TODO: add support for UA1155 (??)
-      contracts.USER_ACTIONS_20.address[Chains.goerli],
-      contracts.USER_ACTIONS_20.abi,
+      contracts.USER_ACTIONS_EPT.address[Chains.goerli],
+      contracts.USER_ACTIONS_EPT.abi,
       web3Provider?.getSigner(),
-    ) as Vault20Actions
+    ) as VaultEPTActions
   }, [web3Provider])
+
+  // Notional User Action: ERC1155
+  // const userActionFC = useMemo(() => {
+  //   return new Contract(
+  //     contracts.USER_ACTIONS_FC.address[Chains.goerli],
+  //     contracts.USER_ACTIONS_FC.abi,
+  //     web3Provider?.getSigner(),
+  //   ) as VaultFCActions
+  // }, [web3Provider])
 
   const approveFIAT = useCallback(
     async (to: string) => {
       if (!userProxy) return
 
       const MAX_AVAILABLE_AMOUNT = ethers.constants.MaxUint256
-      const approveFIAT = userAction.interface.encodeFunctionData('approveFIAT', [
+      // TODO: check if vault/protocol type so we can use EPT or FC
+      const approveFIAT = userActionEPT.interface.encodeFunctionData('approveFIAT', [
         to,
         MAX_AVAILABLE_AMOUNT,
       ])
 
-      const tx = await userProxy.execute(userAction.address, approveFIAT, {
+      const tx = await userProxy.execute(userActionEPT.address, approveFIAT, {
         gasLimit: 1_000_000,
       })
       await tx.wait()
     },
-    [userProxy, userAction.address, userAction.interface],
+    [userProxy, userActionEPT.address, userActionEPT.interface],
   )
 
   const modifyCollateralAndDebt = useCallback(
@@ -85,7 +94,8 @@ export const useUserActions = (): UseUserActions => {
       // @TODO: it is not possible to use this hook when user is not connected nor have created a Proxy
       if (!address || !userProxy || !userProxyAddress) return
 
-      const modifyCollateralAndDebtEncoded = userAction.interface.encodeFunctionData(
+      // TODO: check if vault/protocol type so we can use EPT or FC
+      const modifyCollateralAndDebtEncoded = userActionEPT.interface.encodeFunctionData(
         'modifyCollateralAndDebt',
         [
           params.vault,
@@ -98,12 +108,12 @@ export const useUserActions = (): UseUserActions => {
           params.deltaNormalDebt.toFixed(),
         ],
       )
-      const tx = await userProxy.execute(userAction.address, modifyCollateralAndDebtEncoded, {
+      const tx = await userProxy.execute(userActionEPT.address, modifyCollateralAndDebtEncoded, {
         gasLimit: 1_000_000,
       })
       await tx.wait()
     },
-    [address, userProxy, userProxyAddress, userAction.address, userAction.interface],
+    [address, userProxy, userProxyAddress, userActionEPT.address, userActionEPT.interface],
   )
 
   const depositCollateral = useCallback(
@@ -158,5 +168,5 @@ export const useUserActions = (): UseUserActions => {
     [modifyCollateralAndDebt],
   )
 
-  return { userAction, approveFIAT, depositCollateral, withdrawCollateral, mintFIAT, burnFIAT }
+  return { approveFIAT, depositCollateral, withdrawCollateral, mintFIAT, burnFIAT }
 }
