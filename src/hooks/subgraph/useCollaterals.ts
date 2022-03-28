1ce3c0b0f81b0d6d5b1412efd228cb885c447267
LaimeJesus <jesus.laime@camba.coop>
2022-03-28 09:48:10 -0300

    [Create Position] Runtime errors when calling a contract without an user address (#339)
    * fixes contract errors when fetching collaterals
    
    fixes manage button not syncing in create position page
    
    * compare lower cased addresses, remove unnecessary userAddress variable

--- src/hooks/subgraph/useCollaterals.ts
+++ src/hooks/subgraph/useCollaterals.ts
@@ -1,14 +1,15 @@
 import { usePositionsByUser } from './usePositionsByUser'
 import useSWR from 'swr'
-import { Web3Provider } from '@ethersproject/providers'
+import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
 import _ from 'lodash'
+import { useEffect, useState } from 'react'
 import isDev from '@/src/utils/isDev'
 import { graphqlFetcher } from '@/src/utils/graphqlFetcher'
 import { Collaterals, CollateralsVariables } from '@/types/subgraph/__generated__/Collaterals'
 import { ChainsValues } from '@/src/constants/chains'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 import { COLLATERALS } from '@/src/queries/collaterals'
-import { wrangleCollateral } from '@/src/utils/data/collaterals'
+import { Collateral, wrangleCollateral } from '@/src/utils/data/collaterals'
 
 // TODO Import readonly provider from singleton
 export const fetchCollaterals = ({
@@ -19,10 +20,11 @@ export const fetchCollaterals = ({
 }: {
   protocols?: string[]
   collaterals?: string[]
-  provider: Web3Provider
+  provider: Web3Provider | JsonRpcProvider
   appChainId: ChainsValues
 }) => {
   return graphqlFetcher<Collaterals, CollateralsVariables>(COLLATERALS, {
+    // @TODO: add maturity filter maturity_gte (Date.now()/1000).toString()
     where: { vaultName_in: vaultNames, address_in: userCollaterals },
   }).then(async ({ collateralTypes }) => {
     return Promise.all(
@@ -34,30 +36,38 @@ export const fetchCollaterals = ({
 }
 
 export const useCollaterals = (inMyWallet: boolean, protocols: string[]) => {
-  const { address, appChainId, readOnlyAppProvider, web3Provider } = useWeb3Connection()
+  const { appChainId, readOnlyAppProvider: provider } = useWeb3Connection()
+  const [collaterals, setCollaterals] = useState<Collateral[]>([])
   const { positions } = usePositionsByUser()
 
   // TODO Make this more performante avoiding wrangle of positions or the whole query when inMyWallet is false
-  const collaterals = inMyWallet ? _.uniq(positions.map((p) => p.collateral.address)) : undefined
-
-  const provider = web3Provider ?? (readOnlyAppProvider as Web3Provider)
+  const userPositionCollaterals = inMyWallet
+    ? _.uniq(positions.map((p) => p.collateral.address))
+    : undefined
 
   const { data, error } = useSWR(
-    ['collaterals', collaterals?.join(''), protocols?.join(''), address],
+    ['collaterals', userPositionCollaterals?.join(''), protocols?.join('')],
     () =>
-      provider
-        ? fetchCollaterals({
-            protocols: protocols?.length > 0 ? protocols : undefined,
-            collaterals,
-            provider,
-            appChainId,
-          })
-        : [],
+      fetchCollaterals({
+        protocols: protocols?.length > 0 ? protocols : undefined,
+        collaterals: userPositionCollaterals,
+        provider,
+        appChainId,
+      }),
   )
+  useEffect(() => {
+    const newCollaterals = data?.map((collateral) => {
+      const position = positions.find(
+        (p) => p.collateral.address.toLowerCase() === collateral.address?.toLowerCase(),
+      )
+      return { ...collateral, manageId: position?.id }
+    })
+    setCollaterals(newCollaterals || [])
+  }, [data, positions])
 
   if (isDev() && error) {
     console.error(error)
   }
 
-  return data
+  return collaterals
 }
