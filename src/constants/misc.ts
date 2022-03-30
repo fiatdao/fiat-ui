432ce748e8ac1369deae20be413c512b097d91fe
Shisus <jesus.laime@camba.coop>
2022-03-30 09:26:25 -0300

    rename VIRTUAL_RATE_SAFETY_MARGIN to VIRTUAL_RATE_MAX_SLIPPAGE

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -14,7 +14,7 @@ export const WAIT_BLOCKS = 8
 // @TODO use virtualRate method from publican contract
 export const VIRTUAL_RATE = new BigNumber(1.01)
 // @TODO virtualRateSafetyMargin is a constant in the UI (a margin of 1.000002 reflects the increase in virtualRate for an interest rate of 2% p.a. over 1 hr)
-export const VIRTUAL_RATE_SAFETY_MARGIN = new BigNumber(1.000002)
+export const VIRTUAL_RATE_MAX_SLIPPAGE = new BigNumber(1.000002)
 
 export const INFINITE_BIG_NUMBER = new BigNumber(Number.POSITIVE_INFINITY)
 export const SECONDS_IN_A_YEAR = 31536000
