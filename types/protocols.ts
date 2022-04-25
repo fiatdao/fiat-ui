8eab1a5f20b43fb5c81f26d687094c7828c036eb
fernandomg <fernando.greco@gmail.com>
2022-04-25 14:41:09 +0200

    extend types with Protocols names and keywords

--- types/protocols.ts
+++ types/protocols.ts
@@ -3,9 +3,11 @@ import { contracts } from '@/src/constants/contracts'
 // TODO Move
 export type AppContracts = keyof typeof contracts
 
-export const PROTOCOLS = ['Element'] as const
+export const PROTOCOLS = ['Element', 'Notional', 'BarnBridge'] as const
 export type Protocol = typeof PROTOCOLS[number]
 
-// TODO Add typeguard to check protocol
-
-export const ELEMENT_SUBGRAPH_PROTOCOL = 'VaultEPT'
+export const protocolNamesByKeyword: { [key: string]: Protocol } = {
+  ept: 'Element',
+  fc: 'Notional',
+  sy: 'BarnBridge',
+}
