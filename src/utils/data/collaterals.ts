c7462824a7e28e0be6134673c663ff659bb46760
Luis Gomez <whoislewys@gmail.com>
2022-05-13 15:01:09 -0500

    throw error if collateral cannot be wrangled

--- src/utils/data/collaterals.ts
+++ src/utils/data/collaterals.ts
@@ -1,19 +1,18 @@
-import { getVirtualRate } from '../getVirtualRate'
 import { getFaceValue } from '../getFaceValue'
 import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers'
 import { BigNumber } from 'bignumber.js'
 import { hexToAscii } from 'web3-utils'
-import contractCall from '@/src/utils/contractCall'
-import { stringToDateOrCurrent } from '@/src/utils/dateTime'
-import { Collaterals_collateralTypes as SubgraphCollateral } from '@/types/subgraph/__generated__/Collaterals'
-
+import { getCollateralMetadata } from '@/src/constants/bondTokens'
 import { ChainsValues } from '@/src/constants/chains'
-import { Maybe } from '@/types/utils'
 import { contracts } from '@/src/constants/contracts'
 import { ONE_BIG_NUMBER, WAD_DECIMALS, ZERO_ADDRESS, ZERO_BIG_NUMBER } from '@/src/constants/misc'
-import { Collybus } from '@/types/typechain/Collybus'
+import contractCall from '@/src/utils/contractCall'
+import { stringToDateOrCurrent } from '@/src/utils/dateTime'
 import { getHumanValue } from '@/src/web3/utils'
-import { getCollateralMetadata } from '@/src/constants/bondTokens'
+import { Collaterals_collateralTypes as SubgraphCollateral } from '@/types/subgraph/__generated__/Collaterals'
+import { Publican } from '@/types/typechain'
+import { Collybus } from '@/types/typechain/Collybus'
+import { Maybe } from '@/types/utils'
 
 export type Collateral = {
   id: string
@@ -56,33 +55,53 @@ const wrangleCollateral = async (
     address: { [appChainId]: collybusAddress },
   } = contracts.COLLYBUS
 
-  let currentValue = null
   if (
-    collateral.underlierAddress &&
-    collateral.vault?.address &&
-    collateral.maturity &&
-    collateral.underlierAddress !== ZERO_ADDRESS
+    !collateral.underlierAddress ||
+    !collateral.vault?.address ||
+    !collateral.maturity ||
+    collateral.underlierAddress === ZERO_ADDRESS
   ) {
-    currentValue = await contractCall<Collybus, 'read'>(
-      collybusAddress,
-      collybusAbi,
-      provider,
-      'read',
-      [
-        collateral.vault.address,
-        collateral.underlierAddress,
-        0, // FIXME Check protocol if is not an ERC20?
-        collateral.maturity,
-        false,
-      ],
+    throw new Error(
+      `Cannot wrangle collateral with invalid properties: ${{
+        underlierAddress: collateral.underlierAddress,
+        vault: collateral.vault,
+        maturity: collateral.maturity,
+      }}`,
     )
   }
+
+  const virtualRate = (await contractCall<Publican, 'virtualRate'>(
+    contracts.PUBLICAN.address[appChainId],
+    contracts.PUBLICAN.abi,
+    provider,
+    'virtualRate',
+    [collateral.vault.address],
+  )) as BigNumber | null
+  console.log('virtualrate : ', virtualRate?.toString())
+
+  if (virtualRate === null) {
+    throw new Error(`Error, unable to fetch virtual rate for vault: ${collateral.vault}`)
+  }
+
+  const currentValue = await contractCall<Collybus, 'read'>(
+    collybusAddress,
+    collybusAbi,
+    provider,
+    'read',
+    [
+      collateral.vault.address,
+      collateral.underlierAddress,
+      0, // FIXME Check protocol if is not an ERC20?
+      collateral.maturity,
+      false,
+    ],
+  )
+
   const faceValue = await getFaceValue(
     provider,
     collateral.tokenId ?? 0,
     collateral.vault?.address ?? '',
   )
-  const virtualRate = await getVirtualRate(collateral.vault?.address ?? '', appChainId, provider)
 
   const {
     asset = '',
