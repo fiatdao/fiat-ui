163a46154e401d4aaa5607eb7c634a7e719cdd1a
Luis Gomez <whoislewys@gmail.com>
2022-05-17 18:20:46 -0500

    Fix Protocol Display (#519)
    * fix a bunch of type errors around icons due to different token types
    
    * proper lookup for collateral metadata
    
    * unmemoize getPTokenIconFromMetadata, clean name getter on getVaultAddressesByName
    
    * clean up query to filter out expired vaults
    
    * fix borked icons when switching chains

--- src/components/custom/asset/index.tsx
+++ src/components/custom/asset/index.tsx
@@ -3,19 +3,22 @@ import cn from 'classnames'
 
 import { AssetIcons } from '@/src/components/custom/asset-icons'
 import { getPTokenIconFromMetadata } from '@/src/constants/bondTokens'
+import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
 
 export const Asset: React.FC<{
   className?: string
   mainAsset: string
   title: string
 }> = ({ className, mainAsset, title, ...restProps }) => {
+  const { appChainId } = useWeb3Connection()
+
   return (
     <div className={cn(s.component, className)} {...restProps}>
       <AssetIcons
         className={cn(s.icon)}
         dimensions="30px"
-        mainAsset={getPTokenIconFromMetadata(mainAsset)?.main}
-        secondaryAsset={getPTokenIconFromMetadata(mainAsset)?.secondary}
+        mainAsset={getPTokenIconFromMetadata(appChainId, mainAsset)?.main}
+        secondaryAsset={getPTokenIconFromMetadata(appChainId, mainAsset)?.secondary}
       />
       <div className={cn(s.title)}>{title}</div>
     </div>
