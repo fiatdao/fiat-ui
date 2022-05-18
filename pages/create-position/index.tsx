330ca01fe89adc91c2117d5579db991daa988d03
alex0xbe0 <101981457+alex0xbe0@users.noreply.github.com>
2022-05-18 08:33:57 -0700

    remove field from create position table

--- pages/create-position/index.tsx
+++ pages/create-position/index.tsx
@@ -13,7 +13,7 @@ import SkeletonTable, { SkeletonTableColumnsType } from '@/src/components/custom
 import { WAD_DECIMALS } from '@/src/constants/misc'
 import { useProtocolFilters } from '@/src/hooks/useProtocolFilters'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
-import { Collateral, formatColRatio } from '@/src/utils/data/collaterals'
+import { Collateral } from '@/src/utils/data/collaterals'
 import { parseDate, remainingTime, tablePagination } from '@/src/utils/table'
 import { getHumanValue } from '@/src/web3/utils'
 
@@ -74,14 +74,6 @@ const CreatePosition = () => {
         />
       ),
       title: 'Collateral Value',
-    },
-    {
-      align: 'left',
-      dataIndex: 'vault',
-      render: ({ collateralizationRatio: value }: Collateral['vault']) => {
-        return <CellValue value={value ? `${formatColRatio(value)}%` : '-'} />
-      },
-      title: 'Collateralization Threshold',
     },
     {
       align: 'left',
