a035c419fd5355b374d811cdce2a645a2f9e1ab2
alex0xbe0 <101981457+alex0xbe0@users.noreply.github.com>
2022-05-24 21:43:19 -0700

    clean up build

--- scripts/apollo-schema.sh
+++ scripts/apollo-schema.sh
@@ -14,12 +14,6 @@ then
   exit 1
 fi
 
-npm install -g npx
-
-mkdir types/subgraph
-mkdir types/subgraph/__generated__
-touch $SCHEMA_OUTPUT_FILE
-
 npx apollo service:download --endpoint="$NEXT_PUBLIC_REACT_APP_SUBGRAPH_GOERLI" $SCHEMA_OUTPUT_FILE
 
 npx apollo codegen:generate --localSchemaFile=$SCHEMA_OUTPUT_FILE --target=typescript $GENERATED_OUTPUT_FOLDER --outputFlat --passthroughCustomScalars --customScalarsPrefix=Subgraph_
