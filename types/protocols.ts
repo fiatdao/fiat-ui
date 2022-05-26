3b466586fa7253b7688a508f411e9aa27737fa99
Luis Gomez <whoislewys@gmail.com>
2022-05-26 12:31:15 -0500

    remove old todo

--- types/protocols.ts
+++ types/protocols.ts
@@ -3,7 +3,6 @@ import { contracts } from '@/src/constants/contracts'
 // TODO Move
 export type AppContracts = keyof typeof contracts
 
-// TODO: draw this all from metadata
 export const PROTOCOLS = ['Element', 'Notional', 'BarnBridge'] as const
 export type Protocol = typeof PROTOCOLS[number]
 
