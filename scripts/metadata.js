5fd41ae36cde78f9c8273d62b5521894b220b8e1
Luis Gomez <whoislewys@gmail.com>
2022-06-13 09:55:17 -0500

    bump metadata to new vsn with yield on mainnet

--- scripts/metadata.js
+++ scripts/metadata.js
@@ -6,7 +6,7 @@ const { promisify } = require('util')
 const path = require('path')
 /* eslint-enable  @typescript-eslint/no-var-requires */
 
-const sha = 'abca3b5a10f2d0021b062cc9070dc67d3893e729'
+const sha = '69ac1c4098d8411f776a82659d48d3d4bcea553a'
 
 const metadataFilemap = {
   'metadata-goerli': `https://raw.githubusercontent.com/fiatdao/changelog/${sha}/metadata/metadata-goerli.json`,
