69bef0d2a832f1e8cb692141f07676f3681d6e21
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-06-15 16:56:47 -0500

    removed unnecessary code

--- pages/create-position/[collateralId]/open/index.tsx
+++ pages/create-position/[collateralId]/open/index.tsx
@@ -132,8 +132,7 @@ const FormERC20: React.FC<{
 
   const activeToken = collateral.vault.type === 'NOTIONAL' ? erc1155 : erc20
   const { hasAllowance } = activeToken
-  const { depositCollateral } = useUserActions(collateral.vault.type)
-  const [stateMachine, send] = useMachine(stepperMachine, {
+  const [stateMachine] = useMachine(stepperMachine, {
     context: {
       isProxyAvailable,
       hasAllowance,
