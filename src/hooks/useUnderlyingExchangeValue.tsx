707d792e64c50a196b71880ac92927abded869d6
Luis Gomez <whoislewys@gmail.com>
2022-06-18 10:39:35 -0500

    format

--- src/hooks/useUnderlyingExchangeValue.tsx
+++ src/hooks/useUnderlyingExchangeValue.tsx
@@ -1,30 +1,27 @@
-import BigNumber from 'bignumber.js'
-import { KeyedMutator } from 'swr'
 import { contracts } from '@/src/constants/contracts'
 import useContractCall from '@/src/hooks/contracts/useContractCall'
 import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
-import { BytesLike } from "@ethersproject/bytes";
+import { KeyedMutator } from 'swr'
+import BigNumber from 'bignumber.js'
+import { BytesLike } from '@ethersproject/bytes'
 
 type UnderlierToPToken = {
-  vault: string,
-  balancerVault: string,
-  curvePoolId: BytesLike,
-  underlierAmount: BigNumber,
+  vault: string
+  balancerVault: string
+  curvePoolId: BytesLike
+  underlierAmount: BigNumber
 }
 
-export const useUnderlyingExchangeValue = (params: UnderlierToPToken): [BigNumber, KeyedMutator<any>] => {
+export const useUnderlyingExchangeValue = (
+  params: UnderlierToPToken,
+): [BigNumber, KeyedMutator<any>] => {
   const { appChainId } = useWeb3Connection()
 
   const [underlierToPToken, refetchUnderlierToPToken] = useContractCall(
     contracts.USER_ACTIONS_EPT.address[appChainId],
     contracts.USER_ACTIONS_EPT.abi,
     'underlierToPToken',
-    [
-      params.vault,
-      params.balancerVault,
-      params.curvePoolId,
-      params.underlierAmount.toFixed(0,8)
-    ],
+    [params.vault, params.balancerVault, params.curvePoolId, params.underlierAmount.toFixed(0, 8)],
   ) ?? ['ZERO_BIG_NUMBER', null]
 
   return [underlierToPToken, refetchUnderlierToPToken]
