cd9ec01e04eba9a597954251c19481253ffcc8ce
0xTARC <0xtarc@protonmail.com>
2022-07-05 10:24:02 -0500

    updated scripts to work everytime, for multiple toks

--- scripts/run-testnet.sh
+++ scripts/run-testnet.sh
@@ -6,13 +6,8 @@ if [ -f .env.local ]; then
 fi
 
 yarn run ganache \
-  --deterministic
 	--fork.url=https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY \
 	--miner.defaultGasPrice 30000000000 \
 	--chain.vmErrorsOnRPCResponse=true \
-  --wallet.unlockedAccounts="0xCFFAd3200574698b78f32232aa9D63eABD290703"
-  --wallet.unlockedAccounts="0x16b34ce9a6a6f7fc2dd25ba59bf7308e7b38e186"
-
-# TODO: transfer USDC from whale to 
-# USDC whale 0xCFFAd3200574698b78f32232aa9D63eABD290703
-# DAI 0x16b34ce9a6a6f7fc2dd25ba59bf7308e7b38e186
+  --unlock="0xCFFAd3200574698b78f32232aa9D63eABD290703" \
+  --unlock="0x16b34ce9a6a6f7fc2dd25ba59bf7308e7b38e186"
--- scripts/transfer_localhost_tokens.js
+++ scripts/transfer_localhost_tokens.js
@@ -1,5 +1,5 @@
 /// A script for trasnferring tokens from whales into your own account
-/// when running local mainnet fork with ganache.
+/// when running a local mainnet fork with ganache.
 /// Requires a impersonating the whale accounts with `ganache --wallet.unlockedAccounts="<address_to_impersonate>"`
 
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -9,27 +9,44 @@ const ethers = require('ethers')
 const fs = require('fs')
 
 ;(async () => {
+  const stealWhaleTokens = async (whaleSigner, toAddress, tokenContract, amount) => {
+    const tokenSymbol = await tokenContract.symbol()
+    console.log(`Transferring ${tokenSymbol} tokens to address: `, toAddress)
+    const connectedTokContract = await tokenContract.connect(whaleSigner)
+
+    const tokenDecimals = await connectedTokContract.decimals()
+    const exp = ethers.BigNumber.from(10).pow(tokenDecimals)
+    const bnAmount = ethers.BigNumber.from(amount).mul(exp)
+    await connectedTokContract.transfer(toAddress, bnAmount)
+
+    console.log(
+      `Success! Your ${tokenSymbol} balance is now: ${(
+        await connectedTokContract.balanceOf(toAddress)
+      )
+        .div(exp)
+        .toString()}`,
+    )
+  }
+
   const args = yargs(hideBin(process.argv)).option('to', {
     description: 'The address to send tokens to',
     string: true,
   }).argv
-
   const toAddress = args.to
-  console.log('Transferring USDC tokens to address: ', toAddress)
 
   const erc20Abi = JSON.parse(fs.readFileSync('./src/abis/ERC20.json'))
-
-  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
-  const usdcWhaleSigner = provider.getSigner('0xCFFAd3200574698b78f32232aa9D63eABD290703')
-  const mainnetUSDCAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-  const usdcToken = new ethers.Contract(mainnetUSDCAddress, erc20Abi, usdcWhaleSigner)
-
+  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
   try {
-    const tokenDecimals = await usdcToken.decimals()
-    const exp = ethers.BigNumber.from(10).pow(tokenDecimals)
-    const amount = ethers.BigNumber.from(10000).mul(exp)
-    await usdcToken.transfer(toAddress, amount)
+    const usdcWhaleSigner = provider.getSigner('0xCFFAd3200574698b78f32232aa9D63eABD290703')
+    const mainnetUSDCAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+    const usdcToken = new ethers.Contract(mainnetUSDCAddress, erc20Abi, provider)
+    await stealWhaleTokens(usdcWhaleSigner, toAddress, usdcToken, 10000)
+
+    const daiWhaleSigner = provider.getSigner('0x16b34ce9a6a6f7fc2dd25ba59bf7308e7b38e186')
+    const mainnetDaiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+    const daiToken = new ethers.Contract(mainnetDaiAddress, erc20Abi, provider)
+    await stealWhaleTokens(daiWhaleSigner, toAddress, daiToken, 10000)
   } catch (e) {
-    console.error('Error: ', e)
+    console.error('Error stealing tokens', e)
   }
 })()
