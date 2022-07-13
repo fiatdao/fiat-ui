538f39a2a52b921230251f88a110a4aa4e0f6d57
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-07-13 14:41:19 -0500

    fixed notional market rate

--- src/hooks/useMarketRate.tsx
+++ src/hooks/useMarketRate.tsx
@@ -1,10 +1,10 @@
-import { useUnderlierToFCash } from '@/src/hooks/underlierToFCash'
-import { useUnderlierToFYToken } from '@/src/hooks/useUnderlierToFYToken'
-import { useUnderlierToPToken } from '@/src/hooks/useUnderlierToPToken'
-import BigNumber from 'bignumber.js'
 import { Collateral } from '@/src/utils/data/collaterals'
-import { getNonHumanValue, getHumanValue } from '@/src/web3/utils'
-import { ONE_BIG_NUMBER, ZERO_BIG_NUMBER } from '@/src/constants/misc'
+import { getHumanValue, getNonHumanValue } from '@/src/web3/utils'
+import { ONE_BIG_NUMBER } from '@/src/constants/misc'
+import { useWeb3Connection } from '@/src/providers/web3ConnectionProvider'
+import { contracts } from '@/src/constants/contracts'
+import useContractCall from '@/src/hooks/contracts/useContractCall'
+import BigNumber from 'bignumber.js'
 
 type MarketRateParams = {
   protocol: string // "NOTIONAL" | "YIELD" | "ELEMENT"
@@ -19,47 +19,61 @@ type MarketRateReturnValue = {
 
 export const useMarketRate = (params: MarketRateParams): MarketRateReturnValue => {
   const singleUnderlier = new BigNumber(1)
+  const { appChainId } = useWeb3Connection()
+
+  let contract
+  let abi
+  let contractCall
+  let contractParams
+  let underlierDecimals
+
   switch (params.protocol) {
-    case "ELEMENT": {
-      const [underlierToPToken] = useUnderlierToPToken({
-        vault: params.collateral?.vault?.address ?? '',
-        balancerVault: params.collateral?.eptData?.balancerVault,
-        curvePoolId: params.collateral?.eptData?.poolId,
-        underlierAmount: getNonHumanValue(singleUnderlier, params.underlierDecimals),
-      })
-      return {
-        marketRateTokenScale: underlierToPToken, 
-        marketRateDecimal: ONE_BIG_NUMBER.div(getHumanValue(underlierToPToken, params.underlierDecimals))
-      }
+    case 'ELEMENT': {
+      contract = contracts.USER_ACTIONS_EPT.address[appChainId]
+      abi = contracts.USER_ACTIONS_EPT.abi
+      contractCall = 'underlierToPToken'
+      underlierDecimals = params.underlierDecimals
+
+      const vault = params.collateral?.vault?.address ?? ''
+      const balancerVault = params.collateral?.eptData?.balancerVault
+      const underlierAmount = getNonHumanValue(singleUnderlier, params.underlierDecimals)
+      const curvePoolId = params.collateral?.eptData?.poolId
+      contractParams = [vault, balancerVault, curvePoolId, underlierAmount.toFixed(0, 8)]
+      break
     }
-    case "YIELD": {
-      const [underlierToFYToken] = useUnderlierToFYToken({
-        underlierAmount: getNonHumanValue(singleUnderlier, params.underlierDecimals),
-        yieldSpacePool: params.collateral.fyData.yieldSpacePool,
-      })
-      return {
-        marketRateTokenScale: underlierToFYToken, 
-        marketRateDecimal: ONE_BIG_NUMBER.div(getHumanValue(underlierToFYToken, params.underlierDecimals))
-      }
+    case 'YIELD': {
+      contract = contracts.USER_ACTIONS_FY.address[appChainId]
+      abi = contracts.USER_ACTIONS_FY.abi
+      contractCall = 'underlierToFYToken'
+      underlierDecimals = params.underlierDecimals
+
+      const underlierAmount = getNonHumanValue(singleUnderlier, params.underlierDecimals)
+      const yieldSpacePool = params.collateral.fyData.yieldSpacePool
+      contractParams = [underlierAmount.toFixed(0, 8), yieldSpacePool]
+      break
     }
-    case "NOTIONAL": {
-      const [underlierToFCash] = useUnderlierToFCash({
-        tokenId: params.collateral.tokenId ?? '',
-        amount: getNonHumanValue(singleUnderlier, params.underlierDecimals),
-      })
-      return {
-        marketRateTokenScale: underlierToFCash, 
-        marketRateDecimal: ONE_BIG_NUMBER.div(getHumanValue(underlierToFCash, 8)) //TODO: This needs to not be hardcoded
-      }
+    case 'NOTIONAL': {
+      contract = contracts.USER_ACTIONS_FC.address[appChainId]
+      abi = contracts.USER_ACTIONS_FC.abi
+      contractCall = 'underlierToFCash'
+      underlierDecimals = 8
+
+      const tokenId = params.collateral.tokenId ?? ''
+      const amount = getNonHumanValue(singleUnderlier, params.underlierDecimals)
+      contractParams = [tokenId, amount.toFixed(0, 8)]
+      break
     }
-    default: 
-      return {
-        marketRateTokenScale: ZERO_BIG_NUMBER, 
-        marketRateDecimal: ZERO_BIG_NUMBER
-      }
   }
+
+  const [marketRateTokenScale] = useContractCall(
+    contract || '',
+    abi || '',
+    contractCall || '',
+    contractParams || null,
+  ) ?? ['ZERO_BIG_NUMBER', null]
+
+  const marketRateDecimal = ONE_BIG_NUMBER.div(
+    getHumanValue(marketRateTokenScale, underlierDecimals),
+  )
+  return { marketRateTokenScale, marketRateDecimal }
 }
-
-
-
-
